3e6691ff96127986fa96318b7fbf139d7a8ee0a7
Blackie Tsai <blackie1019@gmail.com>
2017-06-13 16:11:13 +0800

    Update

--- docs/service-worker_sbk.js
+++ docs/service-worker_sbk.js
@@ -11,30 +11,30 @@
     //toolbox.precache(['/cdn1101/images/loading-circle.svg']);
     //.toolbox.options.debug = true;
     // The route for any requests from the googleapis origin
-    toolbox.router.get('/service-worker-demo/images(.*)', toolbox.fastest, {
+    toolbox.router.get('/service-worker-demo/images(.*)', toolbox.cacheFirst, {
         cache: {
             name: 'images',
             maxEntries: 100,
-            maxAgeSeconds: 60
+            maxAgeSeconds: 300
         },
         origin: /blackie1019\.github\.io$/
     });
 
-    toolbox.router.get('/service-worker-demo/css(.*)', toolbox.cacheOnly, {
+    toolbox.router.get('/service-worker-demo/css(.*)', toolbox.cacheFirst, {
         cache: {
             name: 'css',
-            maxEntries: 100,
-            maxAgeSeconds: 300
+            maxEntries: 10,
+            maxAgeSeconds: 1200
         },
         origin: /blackie1019\.github\.io$/
     });
 
     
-    toolbox.router.get('/service-worker-demo/js(.*)', toolbox.cacheOnly, {
+    toolbox.router.get('/service-worker-demo/js(.*)', toolbox.cacheFirst, {
         cache: {
             name: 'js',
-            maxEntries: 100,
-            maxAgeSeconds: 300
+            maxEntries: 10,
+            maxAgeSeconds: 1200
         },
         origin: /blackie1019\.github\.io$/
     });
